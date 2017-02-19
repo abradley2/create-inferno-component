c690e8a64e6a75045c5e0c7e053fd6b2a6d313a1
Tony Bradley <antbradley91@gmail.com>
2017-02-19 12:39:10 -0500

    composability example

--- component.js
+++ component.js
@@ -3,14 +3,16 @@ const hyperx = require('hyperx')
 const html = hyperx(createElement)
 const css = require('sheetify')
 
-const red = css`:host {
-  color: red;
-}`
+const styles = css`
+  :host > h3 {
+    color: red;
+  }
+`
 
 function component (props) {
-  return html`<div>
+  return html`<div class=${styles + (props.styles || '')}>
     <input type='text' value=${props.message} oninput=${onEditMessage} />
-    <h3 class=${red}>${props.message}</h3>
+    <h3>${props.message}</h3>
   </div>`
 
   function onEditMessage (e) {
